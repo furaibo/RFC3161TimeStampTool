e3f710868bcf5f13330973d730d1d2b7951d096d
furaibo <furaiboxyz@gmail.com>
2024-07-21 18:50:39 +0900

    update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+public class Main {
+
+    private static final String PROPERTY_FILE = "resources/app.properties";
+
+    public static void main(String[] args) {
+        System.out.println("Main function");
+    }
+
+    public static Properties getProperties() {
+        Properties property = new Properties();
+        try {
+            property.load(new FileInputStream(PROPERTY_FILE));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            System.exit(1);
+        } catch (IOException e) {
+            System.out.println("IO exception");
+            System.exit(1);
         }
+        return property;
     }
 }
